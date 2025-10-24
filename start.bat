44313d1fd7c4ba570345e4ee429868e27253944f
Matthew-Nale <mnale21@yahoo.com>
2025-10-23 20:08:38 -0400

    Removed duplicate echo

--- start.bat
+++ start.bat
@@ -11,18 +11,17 @@ start "MySQL Server" docker compose up --build
 
 echo Starting Flask Backend...
 
-echo Starting Flask Backend...
-
 cd app
 start "Flask Backend" flask run --debug
 cd ..
 
+echo.
 echo Press any key to end all processes...
 pause >nul
 
 taskkill /IM llama-server.exe /F >nul
 
 docker compose stop
-taskkill /FI "WINDOWTITLE eq MySQL Server" >nul
+taskkill /FI "WINDOWTITLE eq MySQL Server" 
 
 taskkill /FI "WINDOWTITLE eq Flask Backend" >nul
