33e533a1fe6dfb63323dba21b6b4e51c0f2fee94
Matthew-Nale <mnale21@yahoo.com>
2025-10-24 17:28:54 -0400

    Changed order of starting to reduce timeouts

--- start.bat
+++ start.bat
@@ -1,10 +1,6 @@
 @ECHO OFF
 
 echo.
-echo Starting Llama Server...
-
-start "Llama Server" llama-server -m gpt/models/BaseModel.gguf -ngl 33
-
 echo Starting MySQL Docker...
 
 start "MySQL Server" docker compose up --build
@@ -14,6 +10,10 @@ echo Starting Flask Backend...
 cd app
 start "Flask Backend" flask run --debug
 cd ..
+
+echo Starting Llama Server...
+
+start "Llama Server" llama-server -m gpt/models/BaseModel.gguf
 
 echo.
 echo Press any key to end all processes...
