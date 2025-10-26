dc9047b43a739929196b4cff47d03a6ddbc76966
Matthew-Nale <mnale21@yahoo.com>
2025-10-25 20:13:22 -0400

    Locking chat behind further authorization

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,12 +1,31 @@
 from flask import Blueprint, session, redirect, url_for, render_template, current_app
 from flask_discord import requires_authorization, Unauthorized
-from utils.sql import store_discord_tokens
+
+from functools import wraps
+
+from utils.sql import store_discord_tokens, is_whitelisted
 
 auth = Blueprint("auth", __name__)
 
+
+def requires_registration(f):
+    @wraps(f)
+    @requires_authorization
+    def decorated(*args, **kwargs):
+        user_id, username = session.get("user_id"), session.get("discord_username")
+
+        if not user_id:
+            return redirect(url_for("auth.login"))
+        
+        if not is_whitelisted(user_id, username):
+            return "You are not authorized to access this page", 403
+    
+        return f(*args, **kwargs)
+    return decorated
+
+
 @auth.route("/login")
 def login():
-    print(session)
     return current_app.discord.create_session()
 
 @auth.route("/callback")
@@ -16,16 +35,28 @@ def callback():
     user = current_app.discord.fetch_user()
     discord_tokens = session.get('DISCORD_OAUTH2_TOKEN')
 
+    session["user_id"] = user.id
     session["discord_username"] = user.username
     session["logged_in"] = True
 
+    store_discord_tokens(
+        user.id,
+        user.username,
+        user.avatar_url,
+        discord_tokens["access_token"],
+        discord_tokens["refresh_token"],
+        discord_tokens["expires_in"]
+    )
+
     return redirect(url_for("auth.query"))
 
+
 @auth.route("/chat")
-@requires_authorization
+@requires_registration
 def query():
     return render_template("query.html")
 
+
 @auth.errorhandler(Unauthorized)
 def redirect_unauthorized(e):
-    return redirect(url_for("auth.login"))
+    return redirect(url_for("main.index"))
--- app/utils/sql.py
+++ app/utils/sql.py
@@ -1,4 +1,21 @@
-from flask_mysqldb import MySQL
+from .extensions import mysql
 
-def store_discord_tokens():
+def is_whitelisted(discord_id, username):
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """
+        SELECT * FROM WhitelistedUsers
+        WHERE discord_id = %s
+        AND username = %s
+        """,
+        (discord_id, username)
+    )
+
+    result = cur.fetchone()
+    cur.close()
+
+    return bool(result)
+
+
+def store_discord_tokens(id, username, avatar_url, access_token, refresh_token, expires_in):
     return 0
